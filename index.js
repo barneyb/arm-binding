b7c6d32939ad3b1e510b5dbfebf9ae8b532bda7a
barneyb <bboisvert@gmail.com>
2017-03-21 19:34:11 -0700

    ditched babel in favor of plain JS

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-/** equivalent "normal" JavaScript
 MyNum = function(num) {
     this.num = num;
     this.boundGET = this.GET.bind(this);
@@ -8,95 +6,81 @@ MyNum = function(num) {
 MyNum.prototype.GET = function GET() {
     return this.num;
 };
-*/
-class MyNum {
-	constructor(num) {
-    	this.num = num;
-    	this.boundGET = this.GET.bind(this);
-	}
-
-	GET() {
-    	return this.num;
-    }
-}
 
 //////////////////////////////////////////////////////////////////////
 section('1. method()');
 
-const one = new MyNum('one');
+one = new MyNum('one');
 console.log('one', one); // inspect it in the console!
 
-log('one.GET()', () => one.GET()); // one
-log('one.boundGET()', () => one.boundGET()); // one
+log('one.GET()'); // one
+log('one.boundGET()'); // one
 
 //////////////////////////////////////////////////////////////////////
 section('2. hijacked method()');
 
-const two = {
+two = {
     num: 2,
     GET: one.GET,
     boundGET: one.boundGET
 };
 console.log('two', two); // inspect it in the console!
 
-log('two.GET()', () => two.GET()); // 2
-log('two.boundGET()', () => two.boundGET()); // one
+log('two.GET()'); // 2
+log('two.boundGET()'); // one
 
 //////////////////////////////////////////////////////////////////////
 section('3. hijacked method.call()');
 
-log('one.GET.call(two)', () => one.GET.call(two)); // 2
-log('one.boundGET.call(two)', () => one.boundGET.call(two)); // one
+log('one.GET.call(two)'); // 2
+log('one.boundGET.call(two)'); // one
 
 //////////////////////////////////////////////////////////////////////
 section('4. prototype method.call()');
 
-log('MyNum.prototype.GET.call(two)',
-	() => MyNum.prototype.GET.call(two)); // 2
-log('MyNum.prototype.boundGET.call(two)',
-	() => MyNum.prototype.boundGET.call(two)); // error!)
+log('MyNum.prototype.GET.call(two)'); // 2
+log('MyNum.prototype.boundGET.call(two)'); // error!
 
 //////////////////////////////////////////////////////////////////////
 section('5. hijacked prototype method()');
 
-const three = {
+three = {
     num: 3,
     GET: MyNum.prototype.GET,
     boundGET: MyNum.prototype.boundGET // undefined
 };
 console.log('three', three); // inspect it in the console!
 
-log('three.GET()', () => three.GET()); // 3
-log('three.boundGET()', () => three.boundGET()); // error!
+log('three.GET()'); // 3
+log('three.boundGET()'); // error!
 
 //////////////////////////////////////////////////////////////////////
-section('6. ripped instance method()')
+section('6. ripped instance method()');
 
-const fGET = one.GET;
-const fBoundGET = one.boundGET;
+fGET = one.GET;
+fBoundGET = one.boundGET;
 
-log('fGET()', () => fGET()); // error
-log('fBoundGET()', () => fBoundGET()); // one
-
-//////////////////////////////////////////////////////////////////////
-section('7. ripped prototype method()')
-
-const pfGET = MyNum.prototype.GET;
-const pfBoundGET = MyNum.prototype.boundGET;
-
-log('pfGET()', () => pfGET()); // error
-log('pfBoundGET()', () => pfBoundGET()); // error!
+log('fGET()'); // undefined
+log('fBoundGET()'); // one
 
 //////////////////////////////////////////////////////////////////////
-function log(label, work) {
-	let result;
+section('7. ripped prototype method()');
+
+pfGET = MyNum.prototype.GET;
+pfBoundGET = MyNum.prototype.boundGET;
+
+log('pfGET()'); // undefined
+log('pfBoundGET()'); // error!
+
+//////////////////////////////////////////////////////////////////////
+function log(script) {
+	var result;
     try {
-    	result = work();
+    	result = eval(script); // don't look! terrible things!
     } catch (err) {
     	result = err;
     }
-    jQuery("body").append(jQuery('<p><code>' + label
-    	+ '</code> yields <var>' + result + '</var></p>'));
+    jQuery("body").append(jQuery('<p><code>' + script + '</code> yields <var>' + result + '</var></p>'));
 }
 function section(label) {
     jQuery("body").append(jQuery('<h4>' + label + '</h4>'));
